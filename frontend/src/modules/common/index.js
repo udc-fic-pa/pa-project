1ab14caf5d484785f5fab0503b2063f86f1db111
Fernando Bellas Permuy <fbellas@udc.es>
2019-02-14 10:27:21 +0100

    Frontend: se añadió el componente Pager.

--- frontend/src/modules/common/index.js
+++ frontend/src/modules/common/index.js
@@ -3,3 +3,4 @@ export {default as ErrorDialog} from "./components/ErrorDialog";
 export {default as Errors} from "./components/Errors";
 export {default as Loader} from "./components/Loader";
 export {default as Success} from "./components/Success";
+export {default as Pager} from "./components/Pager";

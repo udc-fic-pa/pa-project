53bc51ad513495bf5e03c026a352f8b875bdc55a
Manuel Álvarez Díaz <manuel.alvarez>
2019-02-26 21:57:23 +0100

    Backend: Eliminados imports innecesarios

--- backend/src/main/java/es/udc/paproject/backend/model/entities/User.java
+++ backend/src/main/java/es/udc/paproject/backend/model/entities/User.java
@@ -1,11 +1,9 @@
 package es.udc.paproject.backend.model.entities;
 
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.OneToOne;
 
 @Entity
 public class User {
--- backend/src/test/java/es/udc/paproject/backend/test/model/services/UserServiceTest.java
+++ backend/src/test/java/es/udc/paproject/backend/test/model/services/UserServiceTest.java
@@ -1,9 +1,6 @@
 package es.udc.paproject.backend.test.model.services;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
-
-import javax.transaction.Transactional;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -11,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
 
 import es.udc.paproject.backend.model.common.exceptions.DuplicateInstanceException;
 import es.udc.paproject.backend.model.common.exceptions.InstanceNotFoundException;

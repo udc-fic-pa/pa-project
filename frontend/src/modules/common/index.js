07d8522efe44bb3a93f1660bb3070d34c85cace5
Fernando Bellas Permuy <fbellas@udc.es>
2019-04-10 19:03:22 +0200

    Frontend: se introduce el componente BackLink.

--- frontend/src/modules/common/index.js
+++ frontend/src/modules/common/index.js
@@ -4,3 +4,4 @@ export {default as Errors} from "./components/Errors";
 export {default as Loader} from "./components/Loader";
 export {default as Success} from "./components/Success";
 export {default as Pager} from "./components/Pager";
+export {default as BackLink} from "./components/BackLink";

87dd4fa11b084d600f233bec549830b6097382fe
Fernando <fbellas@udc.es>
2020-01-09 19:20:51 +0100

    Frontend: UpdateProfile.js migrado a componente función.

--- frontend/src/modules/users/components/UpdateProfile.js
+++ frontend/src/modules/users/components/UpdateProfile.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {connect} from 'react-redux';
 import {FormattedMessage} from 'react-intl';
 
@@ -6,136 +6,102 @@ import {Errors} from '../../common';
 import * as actions from '../actions';
 import * as selectors from '../selectors';
 
-class UpdateProfile extends React.Component {
+const UpdateProfile = ({user, updateProfile, history}) => {
 
-    constructor(props) {
+    const [firstName, setFirstName] = useState(user.firstName);
+    const [lastName, setLastName] = useState(user.lastName);
+    const [email, setEmail]  = useState(user.email);
+    const [backendErrors, setBackendErrors] = useState(null);
+    let form;
 
-        super(props);
-
-        this.state = {
-            firstName: props.user.firstName,
-            lastName: props.user.lastName,
-            email: props.user.email,
-            backendErrors: null,
-            passwordsDoNotMatch: false
-        };
-
-    }
-
-    handleFirstNameChange(event) {
-        this.setState({firstName: event.target.value});
-    }
-
-    handleLastNameChange(event) {
-        this.setState({lastName: event.target.value});
-    }
-
-    handleEmailChange(event) {
-        this.setState({email: event.target.value});
-    }
-
-    handleSubmit(event) {
+    const handleSubmit = event => {
 
         event.preventDefault();
 
-        if (this.form.checkValidity()) {
-            this.updateProfile();
+        if (form.checkValidity()) {
+            
+            updateProfile(
+                {id: user.id,
+                firstName: firstName.trim(),
+                lastName: lastName.trim(),
+                email: email.trim()},
+                () => history.push('/'),
+                errors => setBackendErrors(errors));
+
         } else {
-            this.setBackendErrors(null);
-            this.form.classList.add('was-validated');
+
+            setBackendErrors(null);
+            form.classList.add('was-validated');
+
         }
 
     }
 
-    updateProfile() {
-
-        this.props.updateProfile(
-            {id: this.props.user.id,
-            firstName: this.state.firstName.trim(),
-            lastName: this.state.lastName.trim(),
-            email: this.state.email.trim()},
-            () => this.props.history.push('/'),
-            errors => this.setBackendErrors(errors));
-        
-    }
-
-    setBackendErrors(backendErrors) {
-        this.setState({backendErrors});
-    }
-
-    handleErrorsClose() {
-        this.setState({backendErrors: null});
-    }
-
-    render() {
-
-        return (
-            <div>
-                <Errors errors={this.state.backendErrors} onClose={() => this.handleErrorsClose()}/>
-                <div className="card bg-light border-dark">
-                    <h5 className="card-header">
-                        <FormattedMessage id="project.users.UpdateProfile.title"/>
-                    </h5>
-                    <div className="card-body">
-                        <form ref={node => this.form = node} 
-                            className="needs-validation" noValidate onSubmit={(e) => this.handleSubmit(e)}>
-                            <div className="form-group row">
-                                <label htmlFor="firstName" className="col-md-3 col-form-label">
-                                    <FormattedMessage id="project.global.fields.firstName"/>
-                                </label>
-                                <div className="col-md-4">
-                                    <input type="text" id="firstName" className="form-control"
-                                        value={this.state.firstName}
-                                        onChange={(e) => this.handleFirstNameChange(e)}
-                                        autoFocus
-                                        required/>
-                                    <div className="invalid-feedback">
-                                        <FormattedMessage id='project.global.validator.required'/>
-                                    </div>
+    return (
+        <div>
+            <Errors errors={backendErrors} onClose={() => setBackendErrors(null)}/>
+            <div className="card bg-light border-dark">
+                <h5 className="card-header">
+                    <FormattedMessage id="project.users.UpdateProfile.title"/>
+                </h5>
+                <div className="card-body">
+                    <form ref={node => form = node} 
+                        className="needs-validation" noValidate onSubmit={e => handleSubmit(e)}>
+                        <div className="form-group row">
+                            <label htmlFor="firstName" className="col-md-3 col-form-label">
+                                <FormattedMessage id="project.global.fields.firstName"/>
+                            </label>
+                            <div className="col-md-4">
+                                <input type="text" id="firstName" className="form-control"
+                                    value={firstName}
+                                    onChange={e => setFirstName(e.target.value)}
+                                    autoFocus
+                                    required/>
+                                <div className="invalid-feedback">
+                                    <FormattedMessage id='project.global.validator.required'/>
                                 </div>
                             </div>
-                            <div className="form-group row">
-                                <label htmlFor="lastName" className="col-md-3 col-form-label">
-                                    <FormattedMessage id="project.global.fields.lastName"/>
-                                </label>
-                                <div className="col-md-4">
-                                    <input type="text" id="lastName" className="form-control"
-                                        value={this.state.lastName}
-                                        onChange={(e) => this.handleLastNameChange(e)}
-                                        required/>
-                                    <div className="invalid-feedback">
-                                        <FormattedMessage id='project.global.validator.required'/>
-                                    </div>
+                        </div>
+                        <div className="form-group row">
+                            <label htmlFor="lastName" className="col-md-3 col-form-label">
+                                <FormattedMessage id="project.global.fields.lastName"/>
+                            </label>
+                            <div className="col-md-4">
+                                <input type="text" id="lastName" className="form-control"
+                                    value={lastName}
+                                    onChange={e => setLastName(e.target.value)}
+                                    required/>
+                                <div className="invalid-feedback">
+                                    <FormattedMessage id='project.global.validator.required'/>
                                 </div>
                             </div>
-                            <div className="form-group row">
-                                <label htmlFor="email" className="col-md-3 col-form-label">
-                                    <FormattedMessage id="project.global.fields.email"/>
-                                </label>
-                                <div className="col-md-4">
-                                    <input type="email" id="email" className="form-control"
-                                        value={this.state.email}
-                                        onChange={(e) => this.handleEmailChange(e)}
-                                        required/>
-                                    <div className="invalid-feedback">
-                                        <FormattedMessage id='project.global.validator.email'/>
-                                    </div>
+                        </div>
+                        <div className="form-group row">
+                            <label htmlFor="email" className="col-md-3 col-form-label">
+                                <FormattedMessage id="project.global.fields.email"/>
+                            </label>
+                            <div className="col-md-4">
+                                <input type="email" id="email" className="form-control"
+                                    value={email}
+                                    onChange={e => setEmail(e.target.value)}
+                                    required/>
+                                <div className="invalid-feedback">
+                                    <FormattedMessage id='project.global.validator.email'/>
                                 </div>
                             </div>
-                            <div className="form-group row">
-                                <div className="offset-md-3 col-md-1">
-                                    <button type="submit" className="btn btn-primary">
-                                        <FormattedMessage id="project.global.buttons.save"/>
-                                    </button>
-                                </div>
+                        </div>
+                        <div className="form-group row">
+                            <div className="offset-md-3 col-md-1">
+                                <button type="submit" className="btn btn-primary">
+                                    <FormattedMessage id="project.global.buttons.save"/>
+                                </button>
                             </div>
-                        </form>
-                    </div>
+                        </div>
+                    </form>
                 </div>
             </div>
-        );
-
-    }
+        </div>
+    );
 
 }
 

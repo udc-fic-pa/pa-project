7c46ba343016a39f9538717b9851a28f1cb0541c
Fernando Bellas Permuy <fbellas@udc.es>
2020-10-16 13:00:36 +0200

    Actualización al código generado por la versión actual de Create React App.

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -31,11 +31,16 @@ const {locale, messages} = initReactIntl();
 
 /* Render application. */
 ReactDOM.render(
-    <Provider store={store}>
-        <IntlProvider locale={locale} messages={messages}>
-            <App/>
-        </IntlProvider>
-    </Provider>, 
+    <React.StrictMode>
+        <Provider store={store}>
+            <IntlProvider locale={locale} messages={messages}>
+                <App/>
+            </IntlProvider>
+        </Provider>
+    </React.StrictMode>,
     document.getElementById('root'));
 
-serviceWorker.register();
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();

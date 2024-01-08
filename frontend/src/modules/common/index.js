9442417e5ac2c263a59096fd825e4211e161a58c
Fernando Bellas Permuy <fbellas@udc.es>
2024-01-08 11:36:28 +0100

    [Frontend] Se eliminó el componente ConfirmDialog.

--- frontend/src/modules/common/index.js
+++ frontend/src/modules/common/index.js
@@ -1,4 +1,3 @@
-export {default as ConfirmDialog} from "./components/ConfirmDialog";
 export {default as ErrorDialog} from "./components/ErrorDialog";
 export {default as Errors} from "./components/Errors";
 export {default as Success} from "./components/Success";

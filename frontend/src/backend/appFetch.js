fa2058715601891e61104aa0a3720718b8c53f99
Fernando <fbellas@udc.es>
2024-06-06 10:38:19 +0200

    Se introduce appFetch2 para usar async/await.
    Se aplica al caso de uso de búsqueda de productos.

--- frontend/src/backend/appFetch.js
+++ frontend/src/backend/appFetch.js
@@ -5,6 +5,18 @@ const SERVICE_TOKEN_NAME = 'serviceToken';
 let networkErrorCallback;
 let reauthenticationCallback;
 
+export const init = callback => networkErrorCallback = callback;
+
+export const setReauthenticationCallback = callback => reauthenticationCallback = callback;
+
+export const setServiceToken = serviceToken => 
+    sessionStorage.setItem(SERVICE_TOKEN_NAME, serviceToken);
+
+export const getServiceToken = () => sessionStorage.getItem(SERVICE_TOKEN_NAME);
+
+export const removeServiceToken = () => 
+    sessionStorage.removeItem(SERVICE_TOKEN_NAME);
+
 const isJson = response => {
 
     const contentType = response.headers.get("content-type");
@@ -13,6 +25,62 @@ const isJson = response => {
 
 }
 
+const getOptions = (method, body) => {
+
+    const options = {};
+
+    options.method = method;
+
+    if (body) {
+        if (body instanceof FormData) {
+            options.body = body;
+        } else  {
+            options.headers = {'Content-Type': 'application/json'};
+            options.body = JSON.stringify(body);
+        }
+    }
+
+    let serviceToken = getServiceToken();
+
+    if (serviceToken) {
+
+        if (options.headers) {
+            config.headers['Authorization'] = `Bearer ${serviceToken}`;
+        } else {
+            options.headers = {'Authorization': `Bearer ${serviceToken}`};
+        }
+
+    }
+
+    return options;
+
+}
+
+export const appFetch2 = async (method, path, body) => {
+
+    try {
+
+        const response = await fetch(`${import.meta.env.VITE_BACKEND_URL}${path}`, getOptions(method, body));
+        const appFetchResponse = {ok: response.ok, payload: null};
+
+        if (response.status === 401 && reauthenticationCallback){
+            reauthenticationCallback();
+            return appFetchResponse;
+        }
+
+        if (isJson(response)) {
+            appFetchResponse.payload = await response.json();
+        }
+
+       return appFetchResponse;
+
+    } catch (error) {
+        networkErrorCallback();
+    }
+
+}
+
+// FIXME: remove when appFetch2 replaces appFetch.
 const handleOkResponse = (response, onSuccess) => {
 
     if (!response.ok) {
@@ -36,6 +104,7 @@ const handleOkResponse = (response, onSuccess) => {
 
 }
 
+// FIXME: remove when appFetch2 replaces appFetch.
 const handle4xxResponse = (response, onErrors) => {
 
     if (response.status < 400 || response.status >= 500) {
@@ -65,6 +134,7 @@ const handle4xxResponse = (response, onErrors) => {
 
 }
 
+// FIXME: remove when appFetch2 replaces appFetch.
 const handleResponse = (response, onSuccess, onErrors) => {
 
     if (handleOkResponse(response, onSuccess)) {
@@ -79,18 +149,7 @@ const handleResponse = (response, onSuccess, onErrors) => {
     
 }
 
-export const init = callback => networkErrorCallback = callback;
-
-export const setReauthenticationCallback = callback => reauthenticationCallback = callback;
-
-export const setServiceToken = serviceToken => 
-    sessionStorage.setItem(SERVICE_TOKEN_NAME, serviceToken);
-
-export const getServiceToken = () => sessionStorage.getItem(SERVICE_TOKEN_NAME);
-
-export const removeServiceToken = () => 
-    sessionStorage.removeItem(SERVICE_TOKEN_NAME);
-
+// FIXME: remove when appFetch2 replaces appFetch.
 export const config = (method, body) => {
 
     const config = {};
@@ -122,6 +181,7 @@ export const config = (method, body) => {
 
 }
 
+// FIXME: remove when appFetch2 replaces appFetch.
 export const appFetch = (path, options, onSuccess, onErrors) =>
     fetch(`${import.meta.env.VITE_BACKEND_URL}${path}`, options)
         .then(response => handleResponse(response, onSuccess, onErrors))
